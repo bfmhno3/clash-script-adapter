7f5dafdbc10f08ac784e690dd86916436a46e0a6
bfmhno3 <858446559@qq.com>
2026-03-05 22:22:57 +0800

    feat: 配置 DNS 服务，实现无 DNS 泄漏

--- src/clash_verge_rev.js
+++ src/clash_verge_rev.js
@@ -11,9 +11,12 @@ function main(config, profilename) {
   
   // 获取已分组的节点
   const regions = getRegionProxies(proxyNames);
-  
+
   // 注入全局配置并强制生效
   Object.assign(config, buildGlobalSettings());
+
+  // 配置 DNS 服务，防止 DNS 泄露问题
+  config.dns = buildDnsConfig();
 
   return config;
 }
@@ -338,4 +341,335 @@ function getRegionProxies(proxyNames) {
     tw: getProxies(twRegex),
     kr: getProxies(krRegex)
   };
+}
+
+/**
+ * 构建 Clash/Mihomo 的 DNS 服务配置。
+ * 
+ * 该函数返回一份完整的 DNS 配置方案，覆盖缓存算法、FakeIP 模式、
+ * DNS 服务器选择、DNS 污染防护等内容，以防止 DNS 泄露问题。
+ * 支持按照地理位置 (geosite) 和 GEOIP 规则进行分流解析。
+ *
+ * @returns {Object} DNS 配置对象，包含 enable、listen、enhanced-mode、
+ *                   fake-ip-range、nameserver、fallback 等完整配置项
+ */
+function buildDnsConfig() {
+  const config = {
+    // #region ====== enable ===================================================
+
+    // 是否启用，如为 false，则使用系统 DNS 解析。
+    enable: true,
+
+    // #endregion ==============================================================
+
+    // #region ====== cache-algorithm ==========================================
+
+    // 支持的算法：
+    // lru (Least Recently Used)，默认值; 
+    // arc (Adaptive Replacement Cache)。
+    'cache-algorithm': 'arc',
+
+    // #endregion ==============================================================
+
+    // #region ====== prefer-h3 ================================================
+
+    // DOH 优先使用 http/3。
+    'prefer-h3': false,
+
+    // #endregion ==============================================================
+
+    // #region ====== use-hosts ================================================
+
+    // 是否回应配置中的 hosts，默认 true。
+    'use-hosts': true,
+
+    // #endregion ==============================================================
+
+    // #region ====== use-system-hosts =========================================
+
+    // 是否查询系统 hosts，默认 true。
+    'use-system-hosts': true,
+
+    // #endregion ==============================================================
+
+    // #region ====== respect-rules ============================================
+
+    // dns 连接遵守路由规则，需配置 proxy-server-nameserver。
+    // 强烈不建议和 prefer-h3 一起使用！
+    'respect-rules': true,
+
+    // #endregion ==============================================================
+
+    // #region ====== listen ===================================================
+
+    // DNS 服务监听，支持 udp, tcp。
+    'listen': '0.0.0.0:1053',
+
+    // #endregion ==============================================================
+
+    // #region ====== ipv6 =====================================================
+
+    // 是否解析 IPV6，如为 false，则回应 AAAA 的空解析。
+    ipv6: false,
+
+    // #endregion ==============================================================
+
+    // #region ====== default-nameserver =======================================
+
+    // 默认 DNS，用于解析 DNS 服务器的域名。
+    // 必须为 IP，可为加密 DNS。
+    'default-nameserver': ['223.5.5.5'],
+
+    // #endregion ==============================================================
+
+    // #region ====== enhanced-mode ============================================
+
+    // mihomo 的 DNS 处理模式。可选值 fake-ip/redir-host，默认 redir-host。
+    'enhanced-mode': 'fake-ip',
+
+    // #endregion ==============================================================
+
+    // #region ====== fake-ip-range ============================================
+
+    // fakeip 下的 IP 段设置，tun 的默认 IPV4 地址也适用此值作为参考。
+    'fake-ip-range': '198.18.0.1/16',
+
+    // #endregion ==============================================================
+
+    // #region ====== fake-ip-range6 ===========================================
+
+    // fakeip 下的 IPv6 段设置。
+    // 'fake-ip-range6': 'fdfe:dcba:9897::1/64',
+
+    // #endregion ==============================================================
+
+    // #region ====== fake-ip-filter-mode ======================================
+
+    // 可选 blacklist/whitelist/rule，默认 blacklist
+    // whitelist 即只有在匹配成功才返回 fakeip。
+    // 当为 rule 时写法发生改变
+    // 参照 https://wiki.metacubex.one/config/dns/#fake-ip-filter
+    'fake-ip-filter-mode': 'blacklist', 
+
+    // #endregion ==============================================================
+
+    // #region ====== fake-ip-filter ===========================================
+
+    // fakeip 过滤，以下地址不会下发 fakeip 映射用于连接
+    // 值支持域名通配以及引入域名集合
+    // 粘贴复制自 OpenClash 相应配置，感谢开发者的付出
+    'fake-ip-filter': [
+      '*.lan','*.localdomain','*.example','*.invalid','*.localhost','*.test',
+      '*.local','*.home.arpa','*.direct','cable.auth.com',
+      'network-test.debian.org','detectportal.firefox.com',
+      'resolver1.opendns.com','global.turn.twilio.com','global.stun.twilio.com',
+      'app.yinxiang.com','injections.adguard.org','localhost.*.weixin.qq.com',
+      '*.blzstatic.cn','*.cmpassport.com','id6.me','open.e.189.cn',
+      'opencloud.wostore.cn','id.mail.wo.cn','mdn.open.wo.cn','hmrz.wo.cn',
+      'nishub1.10010.com','enrichgw.10010.com','*.wosms.cn','*.jegotrip.com.cn',
+      '*.icitymobile.mobi','*.pingan.com.cn','*.cmbchina.com','*.10099.com.cn',
+      '*.microdone.cn','PDC._msDCS.*.*','DC._msDCS.*.*','GC._msDCS.*.*',
+      // 放行 NTP 服务
+      'time.*.com','time.*.gov','time.*.edu.cn','time.*.apple.com',
+      'time-ios.apple.com','time1.*.com','time2.*.com','time3.*.com',
+      'time4.*.com','time5.*.com','time6.*.com','time7.*.com','ntp.*.com',
+      'ntp1.*.com','ntp2.*.com','ntp3.*.com','ntp4.*.com','ntp5.*.com',
+      'ntp6.*.com','ntp7.*.com','*.time.edu.cn','*.ntp.org.cn','+.pool.ntp.org',
+      'time1.cloud.tencent.com',
+      // 放行网易云音乐
+      'music.163.com','*.music.163.com','*.126.net',
+      // 百度音乐
+      'musicapi.taihe.com','music.taihe.com',
+      // 酷狗音乐
+      'songsearch.kugou.com','trackercdn.kugou.com',
+      // 酷我音乐
+      '*.kuwo.cn',
+      // JOOX 音乐
+      'api-jooxtt.sanook.com','api.joox.com','joox.com',
+      // QQ 音乐
+      'y.qq.com','*.y.qq.com','streamoc.music.tc.qq.com',
+      'mobileoc.music.tc.qq.com','isure.stream.qqmusic.qq.com',
+      'dl.stream.qqmusic.qq.com','aqqmusic.tc.qq.com','amobile.music.tc.qq.com',
+      // 虾米音乐
+      '*.xiami.com',
+      // 咪咕音乐
+      '*.music.migu.cn','music.migu.cn',
+      // Windows 本地连接检测
+      '+.msftconnecttest.com','+.msftncsi.com',
+      // QQ 登录
+      'localhost.ptlogin2.qq.com','localhost.sec.qq.com','+.qq.com',
+      '+.tencent.com',
+      // Game
+      // Nintendo Switch
+      '+.srv.nintendo.net','*.n.n.srv.nintendo.net','+.cdn.nintendo.net',
+      // Sony PlayStation
+      '+.stun.playstation.net',
+      // Microsoft Xbox
+      'xbox.*.*.microsoft.com','*.*.xboxlive.com','xbox.*.microsoft.com',
+      'xnotify.xboxlive.com',
+      // Wotgame
+      '+.battle.net','+.battlenet.com.cn','+.wotgame.cn','+.wggames.cn',
+      '+.wowsgame.cn','+.wargaming.net',
+      // Golang
+      'proxy.golang.org',
+      // STUN
+      'stun.*.*','stun.*.*.*','+.stun.*.*','+.stun.*.*.*','+.stun.*.*.*.*',
+      '+.stun.*.*.*.*.*',
+      // Linksys Router
+      'heartbeat.belkin.com','*.linksys.com','*.linksyssmartwifi.com',
+      // ASUS Router
+      '*.router.asus.com',
+      // Apple Software Update Service
+      'mesu.apple.com','swscan.apple.com','swquery.apple.com',
+      'swdownload.apple.com','swcdn.apple.com','swdist.apple.com',
+      // Google
+      'lens.l.google.com','stun.l.google.com','na.b.g-tun.com',
+      // Netflix
+      '+.nflxvideo.net',
+      // FinalFantasy XIV Worldwide Server & CN Server
+      '*.square-enix.com','*.finalfantasyxiv.com','*.ffxiv.com',
+      '*.ff14.sdo.com','ff.dorado.sdo.com',
+      // Bilibili
+      '*.mcdn.bilivideo.cn',
+      // Disney Plus
+      '+.media.dssott.com',
+      // Shark007 Codecs
+      'shark007.net',
+      // Mijia
+      'Mijia Cloud',
+      // 招商银行
+      '+.cmbchina.com','+.cmbimg.com',
+      // Adguard
+      'local.adguard.org',
+      // 迅雷
+      '+.sandai.net','+.n0808.com',
+      // UU Plugin
+      '+.uu.163.com','ps.res.netease.com',
+      // Wifi Calling
+      '+.pub.3gppnetwork.org',
+      // GEOSITE(Meta core),
+      // geosite:category-games,
+      // geosite:apple-cn,
+      // geosite:google-cn
+    ],
+    
+    // #endregion ==============================================================
+    
+    // #region ====== fake-ip-ttl ==============================================
+
+    // 配置 fakeip 查询返回的 TTL，非必要情况下请勿修改
+    // 'fake-ip-ttl": 1,
+
+    // #endregion ==============================================================
+    
+    // #region ====== nameserver-policy ========================================
+
+    // 指定域名查询的解析服务器，可使用 geosite，优先于 nameserver/fallback 查询
+    'nameserver-policy': {
+      'geosite:cn,private': [
+        'https://223.5.5.5/dns-query',
+        'https://119.29.29.29/dns-query'
+      ],
+      'geosite:geolocation-!cn': [
+        'https://dns.google/dns-query',
+        'https://cloudflare-dns.com/dns-query'
+      ]
+    },
+
+    // #endregion ==============================================================
+    
+    // #region ====== nameserver ===============================================
+
+    // 默认的域名解析服务器
+    nameserver: [
+      'https://223.5.5.5/dns-query',
+      'https://119.29.29.29/dns-query'
+    ],
+
+    // #endregion ==============================================================
+
+    // #region ====== fallback =================================================
+
+    // 后备域名解析服务器，一般情况下使用境外 DNS，保证结果可信。
+    // 配置 fallback 后默认启用 fallback-filter, geoip-code 为 CN。
+    fallback: [
+      'https://dns.google/dns-query',
+      'https://cloudflare-dns.com/dns-query',
+      'tls://8.8.4.4',
+      'tls://1.1.1.1'
+    ],
+
+    // #endregion ==============================================================
+
+    // #region ====== proxy-server-nameserver ==================================
+    
+    // 代理节点域名解析服务器，仅用于解析代理节点的域名
+    // 如果不填则遵循 nameserver-policy、nameserver 和 fallback 的配置。
+    'proxy-server-nameserver': [
+      'https://223.5.5.5/dns-query',
+      'https://119.29.29.29/dns-query'
+    ],
+
+    // #endregion ==============================================================
+    
+    // #region ====== direct-nameserver ========================================
+
+    // 用于 direct 出口域名解析的 DNS 服务器
+    // 如果不填则遵循 nameserver-policy、nameserver 和 fallback 的配置。
+    'direct-nameserver': [
+      'https://223.5.5.5/dns-query',
+      'https://119.29.29.29/dns-query'
+    ],
+
+    // #endregion ==============================================================
+
+    // #region ====== direct-nameserver-follow-policy ==========================
+
+    // 是否遵循 nameserver-policy，默认不遵守，仅当 direct-nameserver 不为空时生效。
+    'direct-nameserver-follow-policy': false,
+
+    // #endregion ==============================================================
+    
+    // #region ====== fallback-filter ==========================================
+
+    // 粘贴复制自 OpenClash 相应配置，感谢开发者的付出
+    // 后备域名解析服务器，满足条件的将使用 fallback 结果或只使用 fallback 解析。
+    'fallback-filter': {
+      // 是否启用 geoip 判断
+      geoip: true,
+
+      // 可选值为国家缩写，默认值为 CN。
+      // 除了 geoip-code 配置的国家 IP，其他的 IP 结果会被视为污染。
+      // geoip-code 配置的国家的结果会直接采用，否则将采用 fallback 结果。
+      'geoip-code': 'CN',
+
+      // 可选值为对应的 geosite 内包含的集合。
+      // geosite 列表的内容被视为已污染
+      // 匹配到 geosite 的域名，将只使用 fallback 解析，不去使用 nameserver。
+      geosite: [ 'gfw' ],
+
+      // 书写内容为 IP/掩码。
+      // 这些网段的结果会被视为污染，
+      // nameserver 解析出这些结果时将采用 fallback 的解析结果。
+      ipcidr: [
+        '::/128','::1/128','2001::/32','0.0.0.0/8','10.0.0.0/8','100.64.0.0/10',
+        '127.0.0.0/8','169.254.0.0/16','172.16.0.0/12','192.0.0.0/24',
+        '192.0.2.0/24','192.88.99.0/24','192.168.0.0/16','198.18.0.0/15',
+        '198.51.100.0/24','203.0.113.0/24','224.0.0.0/4','240.0.0.0/4',
+        '255.255.255.255/32'
+      ],
+
+      // 这些域名被视为已污染，匹配到这些域名，
+      // 会直接使用 fallback 解析，不去使用 nameserver。
+      domain: [
+        '+.google.com','+.facebook.com','+.youtube.com',
+        '+.githubusercontent.com','+.googlevideo.com','+.msftconnecttest.com',
+        '+.msftncsi.com'
+      ]
+    }
+
+    // #endregion ==============================================================
+  };
+
+  return config;
 }
