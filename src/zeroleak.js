b744c4f13ed1413376ad921ef7a652a83fd3de51
bfmhno3 <858446559@qq.com>
2026-03-05 19:31:43 +0800

    feat: 添加转换脚本入口函数

---
+++ src/zeroleak.js
@@ -0,0 +1,12 @@
+/**
+ * Clash/Mihomo 全局转换脚本入口
+ * 负责解析原始配置，注入防 DNS 泄露规则和重写分流策略
+ * 
+ * @param {Object} config - 原始的 Clash/Mihomo 配置对象
+ * @param {string} profilename - 当前订阅配置文件的名称
+ * @returns {Object} 返回注入了无泄漏配置的全新 config 对象
+ */
+function main(config, profilename) {
+
+  return config;
+}
