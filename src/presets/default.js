37ab0dd5e03e8845252e03410f62f5744feb4da9
bfmhno3 <858446559@qq.com>
2026-04-30 13:10:45 +0800

    feat: add default preset entry (6 regions, no TUN)

---
+++ src/presets/default.js
@@ -0,0 +1,26 @@
+import { features } from '../features/default.js';
+import { detectPlatform } from '../modules/platform.js';
+import { getRegionProxies } from '../modules/regions.js';
+import { buildProxyGroups } from '../modules/proxy-groups.js';
+import { buildRules } from '../modules/rules.js';
+import { buildDnsConfig } from '../modules/dns.js';
+import { buildGlobalSettings, buildRuleProviders } from '../modules/global-settings.js';
+import { buildSnifferConfig } from '../modules/network.js';
+
+function main(config, profilename) {
+  const platform = detectPlatform(profilename);
+  const proxyNames = (config.proxies || []).map(p => p.name);
+  const safeProxies = proxyNames.length > 0 ? proxyNames : ['DIRECT'];
+  const regions = getRegionProxies(safeProxies);
+
+  Object.assign(config, buildGlobalSettings(platform));
+  config.dns = buildDnsConfig();
+  config['rule-providers'] = buildRuleProviders();
+  config['proxy-groups'] = buildProxyGroups(regions, safeProxies, features);
+  config.rules = buildRules(features);
+  config.sniffer = buildSnifferConfig();
+
+  return config;
+}
+
+export { main };
